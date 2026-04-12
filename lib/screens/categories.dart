48ebcd64004441cc01dd6469961601bf630b62e6
Paulo <paulotec96@gmail.com>
2026-04-12 18:47:56 -0300

    Refactor CategoriesScreen to filter meals by selected category and update navigation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/models/meal.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => MealsScreen(title: 'Meals', meals: []),
+        builder: (context) =>
+            MealsScreen(title: category.title, meals: filteredMeals),
       ),
     );
   }
@@ -30,8 +37,8 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onSelectCategory: (context, categoryTitle) {
-                _selectCategory(context);
+              onSelectedCategory: () {
+                _selectCategory(context, category);
               },
             ),
         ],
