e6bdd0a9ad4dc950d89479d0b95f918b72c85a0d
Paulo <paulotec96@gmail.com>
2026-04-12 16:39:50 -0300

    Refactor CategoryGridItem to include InkWell for tap handling and improve user interaction

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -8,24 +8,31 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withValues(alpha: 0.7),
-            category.color.withValues(alpha: 0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => Navigator.of(
+        context,
+      ).pushNamed('/category-meals', arguments: category),
+      splashColor: Theme.of(context).primaryColor.withValues(alpha: 0.5),
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withValues(alpha: 0.7),
+              category.color.withValues(alpha: 0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(
-          context,
-        ).textTheme.titleLarge!.copyWith(color: Colors.white),
+        child: Text(
+          category.title,
+          style: Theme.of(
+            context,
+          ).textTheme.titleLarge!.copyWith(color: Colors.white),
+        ),
       ),
     );
   }
