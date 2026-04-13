e7f6a612aa275ebb6621c4b30a49e29e302b9efb
Paulo <paulotec96@gmail.com>
2026-04-13 19:21:29 -0300

    Add FilterScreen widget with gluten-free filter functionality

---
+++ lib/screens/filters.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatefulWidget {
+  static const String id = 'filter_screen';
+
+  const FilterScreen({super.key});
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _glutenFreeFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Your Filters')),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutenFreeFilterSet,
+            onChanged: (newValue) {
+              setState(() {
+                _glutenFreeFilterSet = newValue;
+              });
+            },
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            activeThumbColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
