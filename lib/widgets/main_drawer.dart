d0b755466ede9de1f9050b3a8d2f0000d9722362
Paulo <paulotec96@gmail.com>
2026-04-12 23:09:50 -0300

    Add MainDrawer widget for navigation and app structure

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primary.withValues(alpha: 0.7),
+                  Theme.of(context).colorScheme.primary,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Container(
+              color: Theme.of(context).colorScheme.primary,
+              width: double.infinity,
+              alignment: Alignment.center,
+              child: Text(
+                'Cooking Up!',
+                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
